a57d2ac1971b3d26442cdd544d86bca4bc3da884
Jakob Jordan <jakobjordan@posteo.de>
2019-07-03 03:26:18 +0200

    Remove unused agents

--- lib/agents.py
+++ lib/agents.py
@@ -11,26 +11,3 @@ class FixedStepAgent:
         m = self.dna[self.step]
         self.step += 1
         return m
-
-
-class PositionDependentAgent:
-
-    def __init__(self, dna, level_dict):
-        self.moves = {}
-        for i, state in enumerate(level_dict):
-            if state != 'props' and level_dict[state] not in ('#', '\n'):
-                self.moves[state] = dna[i]
-
-    def next_move(self, state, _):
-        return self.moves[state]
-
-
-# class SensorAgent:
-
-#     def __init__(self, dna, env_states):
-#         self.moves = {}
-#         for i, obs in enumerate(itertools.product(env_states, repeat=4)):
-#             self.moves[obs] = dna[i]
-
-#     def next_move(self, _, obs):
-#         return self.moves[obs]
